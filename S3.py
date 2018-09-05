a4792b771aa226324fc063bd254d1c8cd4113628
Serdar Yagci <srdrygc@gmail.com>
2018-09-05 10:51:10 -0500

    Update S3.py
    AWS Access Key removed

--- S3.py
+++ S3.py
@@ -3,9 +3,9 @@ import boto3
 class S3:
 
     def __init__(self):
-        self.AccessKey = "AKIAIFLQMUSEZLZ5BXVA"
-        self.SecretAccessKey = "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"
-        self.BucketName = "elasticbeanstalk-us-east-2-812104344417"
+        self.AccessKey = ""
+        self.SecretAccessKey = ""
+        self.BucketName = ""
 
     def getAccessKey(self):
         return self.AccessKey
